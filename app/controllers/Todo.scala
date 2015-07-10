ef4b2212d58c8ff34a46681f9e771cc78013c538
Neil Williams <nwillia2@glam.ac.uk>
2015-07-10 18:14:25 +0100

    Added what we worked on

--- app/controllers/Todo.scala
+++ app/controllers/Todo.scala
@@ -2,16 +2,36 @@ package controllers
 
 import java.util
 import java.util.ArrayList
+import javax.inject.Inject
+import play.api.libs.json.{JsArray, JsLookupResult, JsValue}
+import play.api.libs.ws.{WSClient, WSResponse, WSRequest}
 import play.api.mvc.{Action, Controller}
+
+import scala.util.parsing.json.JSONArray
+
+import scala.concurrent.ExecutionContext.Implicits.global
 
 /**
  * Created by nwillia2 on 26/06/2015.
  */
-class Todo extends Controller {
-  def index = Action {
-    val todos: ArrayList[models.Todo] = new util.ArrayList[models.Todo]()
-    todos.add(new models.Todo("Help", "Need some help with scala!"))
-    todos.add(new models.Todo("Try not to give up", "This is rubbish8"))
-    Ok(views.html.todo.index(todos))
+class Todo @Inject() (ws: WSClient) extends Controller {
+
+  def index = Action.async {
+    ws.url("https://api.parse.com/1/classes/Todo")
+      .withHeaders("X-Parse-Application-Id" -> "nVVTfcLwuU5OgI6zB1E72KQGvwKJex8XRzvNZWJ0",
+                   "X-Parse-REST-API-Key" -> "apVhmOkUvHr4ixlYnbE3ryhap79YRts4lgE3Mtlh").get().map {
+        response =>
+          val todos: ArrayList[models.Todo] = new util.ArrayList[models.Todo]()
+          val json: JsValue = response.json
+          val lookup: JsLookupResult = json.\("results")
+          val results: Seq[JsValue] = json.as[JsArray].value
+          for (element: JsValue <- results){
+            val todo: models.Todo = new models.Todo()
+            todo.title = element.\("title").as[String]
+            todo.description = element.\("description").as[String]
+            todos.add(todo)
+          }
+          Ok(views.html.todo.index(todos))
+      }
   }
 }
--- app/models/Todo.scala
+++ app/models/Todo.scala
@@ -3,8 +3,8 @@ package models
 /**
  * Created by nwillia2 on 26/06/2015.
  */
-class Todo(passedTitle: String, passedDescription: String) {
-  var title: String = passedTitle
-  var description: String = passedDescription
+class Todo() {
+  var title: String = ""
+  var description: String = ""
   var completed: Boolean = false
 }
